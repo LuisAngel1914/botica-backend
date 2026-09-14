e3398df5de158dee1514218a785401dbba4d11fc
LuisAngel1914 <i1217924@continental.edu.pe>
2026-09-14 14:27:50 -0500

    feat: relate sales to verified prescriptions

--- app/Models/Venta.php
+++ app/Models/Venta.php
@@ -29,6 +29,11 @@ class Venta extends Model
         return $this->hasMany(DetalleVenta::class, 'venta_id');
     }
 
+    public function receta()
+    {
+        return $this->hasOne(RecetaVenta::class, 'venta_id');
+    }
+
     public function devoluciones()
     {
         return $this->hasMany(DevolucionVenta::class, 'venta_id');
